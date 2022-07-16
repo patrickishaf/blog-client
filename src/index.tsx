82b570e7327affb4135621f970eea017e4d58b0b
onumengine <onumdev@gmail.com>
2022-07-16 22:20:50 +0100

    remove strict mode wrapper from app

--- src/index.tsx
+++ src/index.tsx
@@ -11,13 +11,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path={RouteNames.REGISTER} element={<SignUp/>} />
-        <Route path={RouteNames.LOGIN} element={<Login/>} />
-        <Route path={RouteNames.TIMELINE} element={<Timeline/>} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path={RouteNames.REGISTER} element={<SignUp/>} />
+      <Route path={RouteNames.LOGIN} element={<Login/>} />
+      <Route path={RouteNames.TIMELINE} element={<Timeline/>} />
+    </Routes>
+  </BrowserRouter>
 );
